d5a2511d859ee885e04d3f09dea6c37a01d47b04
r-v-g-7 <r.vinayak.garg77@gmail.com>
2026-01-19 23:37:17 +0530

    Routing for dummy components tested && set Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { BrowserRouter } from "react-router-dom";
+import Login from "./components/login";
+import Feed from "./components/Feed";
+import Register from "./components/Register";
+import Workspace from "./components/Workspace";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <Button className="bg-red-600">Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/Login' element={<Login />}></Route>
+        <Route path='/Feed' element={<Feed />}></Route>
+        <Route path='/Register' element={<Register />}></Route>
+        <Route path='/Workspace' element={<Workspace />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
