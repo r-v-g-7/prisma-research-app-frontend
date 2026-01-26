87b995f52a8ef738d23cb83368d1d7806c69920e
r-v-g-7 <r.vinayak.garg77@gmail.com>
2026-01-26 18:05:16 +0530

    Created auth service

---
+++ src/services/auth.js
@@ -0,0 +1,35 @@
+export const registerUser = async (registerData) => {
+
+    const response = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(registerData)
+    });
+
+    if (!response.ok) {
+        throw new Error("Registration failed");
+    }
+
+    return response.json();
+
+
+}
+
+export const loginUser = async (loginData) => {
+
+    const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(loginData)
+    });
+
+    if (!response.ok) {
+        throw new Error("Login failed");
+    }
+
+    return response.json();
+}
