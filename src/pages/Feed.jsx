110e4f3f2aed553e79859b192154fb008a216bb5
r-v-g-7 <r.vinayak.garg77@gmail.com>
2026-02-06 11:31:29 +0530

    loading state added

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,41 +1,54 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "@/context/AuthContext";
 import { Button } from "@/components/ui/button";
 import PostCard from "@/components/PostCard";
-import { useState } from "react";
-import { useEffect } from "react";
 import { fetchPosts } from "@/services/post";
 
 const Feed = () => {
     const { user, logout } = useContext(AuthContext);
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const loadPosts = async () => {
-            const data = await fetchPosts();
-            setPosts(data.data);
-            console.log(data.data);
-        }
+            try {
+                const data = await fetchPosts();
+                setPosts(data.data);
+                setLoading(false);
+                console.log(data.data);
+            } catch (err) {
+                console.error(err.message);
+                setLoading(false);
+            }
+
+        };
         loadPosts();
     }, []);
 
-
     return (
-        <div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg p-6 flex flex-col gap-4">
-            <h1 className="text-2xl font-semibold text-gray-800">
-                Welcome to Feed, {user?.name}
-            </h1>
+        loading ? <p>Loading .....</p> :
+            <div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg p-6 flex flex-col gap-4">
+                <h1 className="text-2xl font-semibold text-gray-800">
+                    Welcome to Feed, {user?.name}
+                </h1>
 
-            <Button
-                onClick={logout}
-                className="bg-red-500 text-white hover:bg-red-600 w-32 py-2"
-            >
-                Logout
-            </Button>
-            {posts && posts.map((post) => (
-                <PostCard post={post} key={post._id + 1} />
-            ))}
-        </div>
+                <Button
+                    onClick={logout}
+                    className="bg-red-500 text-white hover:bg-red-600 w-32 py-2"
+                >
+                    Logout
+                </Button>
+
+                <div className="flex flex-col gap-4">
+                    {posts.length === 0 ? (
+                        <p className="text-gray-500 text-center">No posts yet 🚫</p>
+                    ) : (
+                        posts.map((post) => (
+                            <PostCard post={post} key={post._id} />
+                        ))
+                    )}
+                </div>
+            </div>
     );
 };
 
